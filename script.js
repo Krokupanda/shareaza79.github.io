7c6e44562d5aa51e8113fcb9230cc1a1ddb8b1e3
Alexander <krokupanda@gmail.com>
2022-05-15 22:08:05 +0200

    not desired

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-document.body.innerHTML = '<h2>Hello from JS</h2>'
+//document.body.innerHTML = '<h2>Hello from JS</h2>'
+let elem = document.createElement('div');
+elem.className = "add";
+elem.innerHTML = "<h2>Nested from JS</h2>";
+document.body.append(elem);
